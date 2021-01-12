8fd6d22c944089ff5e2569ec5e169757f7095745
edikedik <edikedikedikedik@gmail.com>
2021-01-12 08:28:02 +0100

    genetic initial

--- protmc/pairwise/__init__.py
+++ protmc/pairwise/__init__.py
@@ -1,1 +1,2 @@
 from .search import AffinityWorker, AffinitySearch, infer_mut_space
+from .genetic import Genetic, GeneticParams, ParsingParams, prepare_data
--- setup.py
+++ setup.py
@@ -4,10 +4,22 @@ from setuptools import find_packages
 
 setup(
     name='ProteusTools',
-    version='0.1dev2',
+    version='0.1dev3',
     author='Ivan Reveguk',
+    author_email='ivan.reveguk@polytechnique.edu',
+    description='A (growing) collection of high-level interface tools handling protmc',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Linux",
+    ],
+    python_requires='>=3.6',
     packages=find_packages(),
+    # TODO: verify packages' versions
     install_requires=[
+        'ray',
+        'genetic',
+        'numba==0.51.2',
         'click>=7.1.2',
         'pandas>=1.1.3',
         'tqdm>=4.50.2',
