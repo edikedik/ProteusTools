57c8cca096c1f4f094a59fae5f315708097760be
edikedik <edikedikedikedik@gmail.com>
2021-03-24 11:40:39 +0100

    revert to regular logging

--- protmc/operators/flattener.py
+++ protmc/operators/flattener.py
@@ -10,14 +10,10 @@ class Flattener(AbstractPoolExecutor):
     def __init__(self, id_: Id = None, reference: t.Optional[str] = None,
                  predicate_mc: t.Optional[t.Callable[[MC], bool]] = None,
                  predicate_adapt: t.Optional[t.Callable[[ADAPT], bool]] = None,
-                 count_threshold: int = 1, log_path: t.Optional[str] = None, loglevel=logging.INFO):
+                 count_threshold: int = 1):
         super().__init__(id_)
         self.predicate_mc = predicate_mc
         self.predicate_adapt = predicate_adapt
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(loglevel)
-        if log_path is not None:
-            self.logger.addHandler(logging.FileHandler(log_path, 'a+'))
         self.reference = reference
         self.count_threshold = count_threshold
 
@@ -43,12 +39,12 @@ class Flattener(AbstractPoolExecutor):
         # Dump the last bias' step the into working dirs of both workers
         adapt_bias_path = f'{adapt.params.working_dir}/{adapt.params.input_bias_name}'
         adapt.bias.dump(adapt_bias_path, last_step=True, tsv=False)
-        self.logger.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- '
+        logging.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- '
                          f'saved current bias to {adapt_bias_path}')
         mc_bias_path = f'{mc.params.working_dir}/{mc.params.input_bias_name}'
         # Copying is faster than calling `dump`
         copyfile(adapt_bias_path, mc_bias_path)
-        self.logger.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- '
+        logging.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- '
                          f'transferred bias from {adapt_bias_path} to {mc_bias_path}')
         # Modify configs to use dumped bias
         adapt.modify_config(field_setters=[('MC_IO', 'Bias_Input_File', adapt_bias_path)], dump=True)
@@ -59,25 +55,25 @@ class Flattener(AbstractPoolExecutor):
         adapt, mc = pool
         step = 0
         if self.adapt_passes(adapt) and self.mc_passes(mc):
-            self.logger.error(f'Both workers appear to be flattened at step 0. '
+            logging.error(f'Both workers appear to be flattened at step 0. '
                               f'Check your stopping criteria')
         while not self.adapt_passes(adapt) or not self.mc_passes(mc):
-            self.logger.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- starting step {step}')
+            logging.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- starting step {step}')
             try:
                 adapt = executor(adapt)
             except Exception as e:
                 msg = f'Executor {executor.id} failed to execute {adapt.id} with an error {e}'
-                self.logger.error(msg)
+                logging.error(msg)
                 raise ValueError(msg)
             self.transfer_bias(adapt, mc)
             try:
                 mc = mc_executor(mc) if mc_executor else executor(mc)
             except Exception as e:
                 msg = f'Executor {mc_executor.id} failed to execute {mc.id} with an error {e}'
-                self.logger.error(msg)
+                logging.error(msg)
                 raise ValueError(msg)
             step += 1
-        self.logger.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- finished at step {step}')
+        logging.info(f'Flattener {self.id} of {adapt.id} and {mc.id} -- finished at step {step}')
         return adapt, mc
 
 
