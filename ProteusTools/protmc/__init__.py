88ae10734bc00f0b2dad9e85ba6a4f8479d78a0e
edikedik <edikedikedikedik@gmail.com>
2021-04-27 18:56:45 +0300

    fix paths

---
+++ ProteusTools/protmc/__init__.py
@@ -0,0 +1,9 @@
+from importlib import resources
+from . import resources as rsc
+
+with resources.path('resources', 'protMC.exe') as path:
+    PROTMC_PATH = path.name
+with resources.path('resources', 'ADAPT.conf') as path:
+    ADAPT_CONF_PATH = path.name
+with resources.path('resources', 'MC.conf') as path:
+    MC_CONF_PATH = path.name
--- ProteusTools/protmc/resources/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib import resources
-
-with resources.path('resources', 'protMC.exe') as path:
-    PROTMC_PATH = path.name
-with resources.path('resources', 'ADAPT.conf') as path:
-    ADAPT_CONF_PATH = path.name
-with resources.path('resources', 'MC.conf') as path:
-    MC_CONF_PATH = path.name
